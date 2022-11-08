f4e1c1ffb49b4c86b02e2790e657a3ac61d736e9
enversanli <enver@17grad.com>
2022-11-08 23:01:38 +0100

    Some updates

--- resources/js/components/SearchBoxComponent.vue
+++ resources/js/components/SearchBoxComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <form action="/etkinlik-ara">
-        <h2 clasS="text-center text-2xl">Aradığın Tüm Etkinlikler Netsekki'de !</h2>
+        <h2 clasS="text-center text-2xl">Aradığın Tüm Etkinlikler Berlindeyiz.de 'de !</h2>
         <div class="grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-1 text-lg text-theme-color mt-10 w-2/3 mx-auto">
 
           <select name="kategori"
@@ -9,11 +9,11 @@
             <option value="">Kategori</option>
             <option v-for="category in categories" :value="category.slug">{{ category.name }}</option>
           </select>
-          <select name="sehir"
+          <!--<select name="sehir"
                   class="h-9 m-3 p-1 bg-white shadow-md rounded-lg transition duration-700 hover:shadow-xl cursor-pointer animate__animated animate__fadeIn">
             <option value="">Şehir</option>
             <option v-for="city in cities" :value="city.slug">{{ city.name }}</option>
-          </select>
+          </select>-->
           <select
               @change="selectDate($event)"
               class="h-9  m-3 p-1 bg-white shadow-md rounded-lg transition duration-700 hover:shadow-xl cursor-pointer animate__animated animate__fadeIn ">
