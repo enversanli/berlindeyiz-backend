fff473f27bfe1e7f4bd412ffb6d1bfc0a68abfbb
enversanli <enver@17grad.com>
2022-11-08 23:33:54 +0100

    genel2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,10 @@ class CreateUsersTable extends Migration
     Schema::create('users', function (Blueprint $table) {
       $table->id();
       $table->enum('role', ['user', 'admin', 'organizer'])->default(\App\Support\Enum\UserRolesEnum::ORGANIZER);
-      $table->foreign('organizer_id')->constrained();
-      $table->foreign('business_id')->constrained();
-      $table->foreign('city_id')->constrained();
-      $table->foreign('district_id')->constrained();
+      $table->integer('organizer_id')->nullable();
+      $table->foreign('business_id')->constrained('businesses');
+      $table->foreign('city_id')->constrained('cities');
+      $table->foreign('district_id')->constrained('districts');
       $table->string('first_name');
       $table->string('last_name');
       $table->string('email')->unique();
@@ -31,7 +31,7 @@ class CreateUsersTable extends Migration
       $table->string('mobile_phone')->nullable();
       $table->string('office_phone')->nullable();
       $table->string('photo')->nullable();
-      $table->string('status')->default(\App\Support\Enum\UserStatusEnum::MAIL_VERIFICATION);
+      $table->enum('status')->default(\App\Support\Enum\UserStatusEnum::MAIL_VERIFICATION);
       $table->rememberToken();
       $table->timestamps();
       $table->softDeletes();
