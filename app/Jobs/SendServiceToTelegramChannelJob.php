d35fbb893b94004028049e9ab2a6a2d3b55ad2c4
enversanli <enver@17grad.com>
2022-11-11 00:53:25 +0100

    telegram job

---
+++ app/Jobs/SendServiceToTelegramChannelJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Service;
+use Facades\App\Services\TelegramService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendServiceToTelegramChannelJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $service;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Service  $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $text = $this->service->title;
+        $image = $this->service->image;
+
+      TelegramService::sendMessage($text, $image);
+    }
+}
