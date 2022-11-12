deeb9e992d0c2619db96ec9e8fdb092215409aad
enversanli <enver@17grad.com>
2022-11-13 00:09:18 +0100

    Service list 5

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -68,7 +68,7 @@ class ServiceController extends Controller
 
 
         $services = $services
-          ->orderBy('date_from', 'ASC')
+          ->orderBy('date_from', 'DESC')
           ->orderBy('status', 'ASC')
             ->paginate(10);
 
