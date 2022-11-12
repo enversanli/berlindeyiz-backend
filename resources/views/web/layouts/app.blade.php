bfcbd60e944fbca271a9edc45bf28ddeb4e25331
enversanli <enver@17grad.com>
2022-11-12 19:03:32 +0100

    social med icon

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -87,14 +87,13 @@
                 </ul>
             </div>
             <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Bizi Takip
-                    Edin</h2>
+                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Bizi Takip Edin</h2>
                 <ul class="text-gray-600 dark:text-gray-400">
                     <li class="mb-4">
-                        <a href="/" class="hover:underline ">Instagram</a>
+                        <a href="https://www.facebook.com/profile.php?id=100087378266298" class="hover:underline ">Instagram</a>
                     </li>
                     <li>
-                        <a href="/" class="hover:underline">Facebook</a>
+                        <a href="https://www.instagram.com/berlindeyiz.de/?igshid=YmMyMTA2M2Y%3D" class="hover:underline">Facebook</a>
                     </li>
                 </ul>
             </div>
@@ -103,10 +102,10 @@
                     Haklarınız</h2>
                 <ul class="text-gray-600 dark:text-gray-400">
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Gizlilik Politikası</a>
+                        <a href="/" class="hover:underline">Gizlilik Politikası</a>
                     </li>
                     <li>
-                        <a href="#" class="hover:underline">Hüküm &amp; Koşullar</a>
+                        <a href="/" class="hover:underline">Hüküm &amp; Koşullar</a>
                     </li>
                 </ul>
             </div>
@@ -118,7 +117,7 @@
                                                                                         class="hover:underline">Berlideyiz™</a>. Tüm Hakları Saklıdır.
         </span>
         <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-            <a href="https://www.facebook.com/profile.php?id=100087378266298" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <a href="https://www.facebook.com/profile.php?id=100087378266298" target="_blank" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                     <path fill-rule="evenodd"
                           d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
@@ -143,7 +142,6 @@
         </div>
     </div>
     <div class="fixed right-2 bottom-2 bg-theme-color z-10 p-2.5 rounded-full hover:cursor-pointer">
-
         <a href="https://api.whatsapp.com/send?phone=4917676198353" target="_blank" class="w-full h-full">
             <svg
                     class="w-8 h-8 text-black-400 fill-current"
