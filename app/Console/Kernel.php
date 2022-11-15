52c7b554279e11581dc2395cc4924b526aab085d
enversanli <enver@17grad.com>
2022-11-15 23:57:33 +0100

    removed command schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,6 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
       $schedule->command('services:daily-reminder')->dailyAt('18:00');
-      $schedule->command('services:daily-reminder')->everyMinute();
     }
 
     /**
