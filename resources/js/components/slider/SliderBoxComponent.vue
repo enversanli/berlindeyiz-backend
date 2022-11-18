ed35143374987040dba19fd1a72eac6a41c1d565
enversanli <enver@17grad.com>
2022-11-19 00:53:02 +0100

    carousel

--- resources/js/components/slider/SliderBoxComponent.vue
+++ resources/js/components/slider/SliderBoxComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <h2 class="text-xl my-3">{{ title }}</h2>
-    <carousel :autoplay="true" :loop="true" :items="3" :autoplaySpeed="1400" :dots="false" :nav="false" :margin="5"
-              class="w-full animate__animated animate__fadeIn shadow shadow-sm mb-10 h-50" v-if="rows.length > 0">
-      <div class="w-full h-52 overflow-hidden relative" v-for="row in rows">
+    <carousel :autoplay="true" :loop="true" :items="3" :responsive="{0:{items:1,nav:false},600:{items:3,nav:false}}" :autoplaySpeed="1400" :dots="false" :nav="false" :margin="5"
+              class="w-full animate__animated animate__fadeIn shadow shadow-sm mb-10 h-60" v-if="rows.length > 0">
+      <div class="w-full h-60 overflow-hidden relative" v-for="row in rows">
         <a :href="'/etkinlikler/' + row.slug">
-          <img class="w-full h-52"
+          <img class="w-full h-60"
                :alt="row.title"
                :src="'/storage/' + row.logo">
 
