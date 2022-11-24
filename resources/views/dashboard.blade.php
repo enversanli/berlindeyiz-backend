3aaeeb8c2a6c7200796bd86431b313b3a9173699
enversanli <enver@17grad.com>
2022-11-24 09:09:07 +0100

    welcome message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in! <br><b>Tulpar !</b>
+                    Hoş geldin, <br><b>{{auth()->user()->first_name}}</b>
 {{--                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">--}}
 {{--                        <div class="p-6 bg-white border-b border-gray-200" id="counter">--}}
 {{--                            <div class="dashboard-list">--}}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!!') }}
+                    {{ __('Hoş Geldin !') }}
                 </div>
             </div>
         </div>
