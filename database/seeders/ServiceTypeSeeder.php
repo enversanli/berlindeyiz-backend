dfdc06324d8d5a0f94b628d6c13d600eb7608036
enversanli <enver@17grad.com>
2022-12-16 22:42:24 +0100

    service type seeder added2

---
+++ database/seeders/ServiceTypeSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class ServiceTypeSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $serviceTypes = [
+      [
+        'title' => 'Etkinlikler',
+        'description' => ''
+      ],
+      [
+        'title' => 'Doktorlar',
+        'description' => ''
+      ],
+      [
+        'title' => 'Avukatlar',
+        'description' => ''
+      ]
+    ];
+
+    foreach ($serviceTypes as $serviceType) {
+      $slug = Str::slug($serviceType['title']);
+
+      if (Type::where('slug', $slug)->exists()) {
+        continue;
+      }
+
+      Type::create([
+        'title' => $serviceType['title'],
+        'slug' => $slug,
+        'text' => $serviceType['description'],
+      ]);
+    }
+
+  }
+}
