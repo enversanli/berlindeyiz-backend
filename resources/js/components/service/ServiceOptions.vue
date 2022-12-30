22c508848ff8e8481db6cc3a194139097e078ce8
enversanli <enver@17grad.com>
2022-12-30 20:37:07 +0100

    Service detail options fixed2

--- resources/js/components/service/ServiceOptions.vue
+++ resources/js/components/service/ServiceOptions.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-auto mr-auto ml-auto p-3  z-50 position-fixed bottom-2 right-3 left-3 text-center">
-    <a v-if="service.meta.ticket" :href="'tel:' + service.meta.ticket" target="_blank" class="w-32 p-3 shadow-lg bg-theme-color mr-1">
+    <a v-if="service.meta.ticket" :href="service.meta.ticket" target="_blank" class="w-32 p-3 shadow-lg bg-theme-color mr-1">
       <i class="fas fa-shopping-cart"></i> Bilet Al
     </a>
     <a v-if="service.meta.mail" :href="'mailto:' + service.meta.mail" target="_blank" class="bg-theme-color p-3 mr-1">
