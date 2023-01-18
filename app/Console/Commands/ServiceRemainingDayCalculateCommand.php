8eee8eaa48b93f856a2c9824f5f5dcb8fd2393d1
enversanli <enver@17grad.com>
2023-01-19 00:44:44 +0100

    Service remaining day 2

--- app/Console/Commands/ServiceRemainingDayCalculateCommand.php
+++ app/Console/Commands/ServiceRemainingDayCalculateCommand.php
@@ -29,7 +29,10 @@ class ServiceRemainingDayCalculateCommand extends Command
           $remainingDay = $today->diffInDays($serviceStartDate);
 
           $service->update(['remaining_day' => $remainingDay]);
+
+          $this->info("$service->tite is calculated...");
         }
 
+        $this->info('Completed.');
     }
 }
