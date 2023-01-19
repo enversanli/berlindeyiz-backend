58c81f81f091142bdfc2980c7f5aa2afbc6735c2
enversanli <enver@17grad.com>
2023-01-19 01:07:55 +0100

    update repeating command added to kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
       $schedule->command('services:daily-reminder')->dailyAt('18:00');
       $schedule->command('services:remaining-day-calculate')->dailyAt('00:1');
+      $schedule->command('services:update-repeating')->dailyAt('01:00');
       $schedule->command('services:update-finished')->twiceDaily(13, 1);
     }
 
