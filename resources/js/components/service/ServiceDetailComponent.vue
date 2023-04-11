b71cb5ca430b2cd7ee163bfe87c3803d28e6d707
enversanli <enver@17grad.com>
2023-04-12 00:30:07 +0200

    internal ticket 2

--- resources/js/components/service/ServiceDetailComponent.vue
+++ resources/js/components/service/ServiceDetailComponent.vue
@@ -142,12 +142,6 @@
               <img src="/images/sponsors/enversanli.png" class="h-auto mr-auto ml-auto" alt="Berlin Yazılım Uzmamı">
             </a>
           </div>
-          <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/JlJetGPOso0"
-                    title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowfullscreen></iframe>
-          </div>
         </div>
 
       </div>
