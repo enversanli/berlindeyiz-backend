20579a6f7d9cfe4f0e6f0a76ae38543e6ca9375d
enversanli <en.sanli@hotmail.com>
2024-02-18 15:28:32 +0100

    order by updated

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,7 +7,7 @@ use App\Models\Ticket;
 class DashboardController
 {
   public function index(){
-    $tickets = Ticket::orderBy('created_at', 'ASC')->with('service')->take(20)->get();
+    $tickets = Ticket::orderBy('created_at', 'DESC')->with('service')->take(20)->get();
 
     return view('dashboard')->with('tickets', $tickets);
   }
