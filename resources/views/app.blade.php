cf33c21b279fd01fcf37c2996de0c427d15a1ee6
enversanli <en.sanli@hotmail.com>
2024-02-24 18:13:03 +0100

    no index

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="description" content="Berlindeyiz, Berlin'e dair müzik, kültür, sanat, edebiyat ve tüm etkinlikleri kolayca bulmanızı sağlar.">
+        <meta name="robots" content="noindex,nofollow">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
