3fae418ccf4928b9130165a5ab952552a4540e45
enversanli <en.sanli@hotmail.com>
2024-02-29 01:04:49 +0100

    testing something

--- app/Console/Commands/SiteMapGeneratorCommand.php
+++ app/Console/Commands/SiteMapGeneratorCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Service;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Storage;
 use Spatie\Sitemap\SitemapGenerator;
 use Spatie\Sitemap\Tags\Url;
 
@@ -38,7 +39,7 @@ class SiteMapGeneratorCommand extends Command
 
     $filePath = App::isLocal() ? 'sitemap.xml' : '/var/www/berlindeyiz-front/pages/sitemap.xml.vue';
 
-    $otherPages = [
+      $otherPages = [
       '/sikca-sorulan-sorular',
       '/duyurular',
       '/doktorlar',
@@ -47,29 +48,38 @@ class SiteMapGeneratorCommand extends Command
       '/turk-avukatlari',
     ];
 
-    $counter = 0;
-    $this->error('Basliyor...');
-    foreach ($otherPages as $otherPage) {
+      $counter = 0;
+      $this->error('Basliyor...');
+      foreach ($otherPages as $otherPage) {
         $fullUrl = $mainUrl . '/'.$otherPage;
 
-      $sitemapGenerator->add(Url::create($fullUrl)->setPriority(0.5));
-      $this->info('Added To Sitemap : ' . $otherPage);
-      $counter++;
+        $sitemapGenerator->add(Url::create($fullUrl)->setPriority(0.5));
+        $this->info('Added To Sitemap : ' . $otherPage);
+        $counter++;
     }
 
-    foreach ($services as $service) {
-      $url = $mainUrl . '/'.$service->type->slug . '/' . $service->slug;
+      foreach ($services as $service) {
+        $url = $mainUrl . '/'.$service->type->slug . '/' . $service->slug;
 
-      $sitemapGenerator->add(Url::create($url)->setPriority(0.5));
-      $this->info('Added To Sitemap : ' . $url);
-      $counter++;
+        $sitemapGenerator->add(Url::create($url)->setPriority(0.5));
+        $this->info('Added To Sitemap : ' . $url);
+        $counter++;
     }
 
+      if (!App::isLocal()){
+          $renderedContent = $sitemapGenerator->render();
+          $renderedContent = str_replace('<?xml version="1.0" encoding="UTF-8"?>', '', $renderedContent);
+          $renderedContent = str_replace('<urlset xmlns=', '<template><urlset xmlns=', $renderedContent);
+          $renderedContent = str_replace("</urlset>", '</urlset></template>', $renderedContent);
+          Storage::disk('front')->put('/public/sitemap.xml.vue', $renderedContent);
+      }else{
+          $sitemapGenerator->
+          writeToFile($filePath);
+      }
 
-    $sitemapGenerator->
-    writeToFile($filePath);
 
-    $this->error('Sitemap Genereted, Please check...');
+
+      $this->error('Sitemap Genereted, Please check...');
     $this->info("{$counter} page stored to sitemap.");
   }
 }
